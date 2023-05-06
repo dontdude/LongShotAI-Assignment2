2d63f1150089948bcce31a21bef2fb5ae420ed8d
Chandan Mishra <imchandan1947@gmail.com>
2023-05-06 21:16:53 +0530

    drag and drop feature applied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {
+  DndContext,
+  closestCenter
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  verticalListSortingStrategy
+} from "@dnd-kit/sortable";
+import { SortableItem } from './SortableItem';
 
 function App() {
+  const [outlines, setOutlines ] = useState([
+    'Shopping in Barcelona',
+    'Famous Shopping Streets',
+    'Shopping malls',
+    'Sales Tax Refunds',
+    'You may also be interested in',
+    'Where to Stay',
+  ]);
+  
+  function handleDragEnd(event) {
+    console.log("Drag end called");
+    const {active, over} = event;
+    console.log("ACTIVE: " + active.id);
+    console.log("OVER :" + over.id);
+
+    if(active.id !== over.id) {
+      setOutlines((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+      });
+      
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <div className="h-screen w-6/12 p-4 m-auto">
+        <h3>Edit outlines!</h3>
+        <span className="text-gray-500">Outlines</span>
+        <div className="p-3 flex flex-col justify-center">
+          <SortableContext
+            items={outlines}
+            strategy={verticalListSortingStrategy}
+          >
+            {/* We need components that use the useSortable hook */}
+            {outlines.map(outline => <SortableItem key={outline} id={outline}/>)}
+          </SortableContext>
+          <button className="text-purple-600 border border-purple-600 rounded-lg text-lg py-2 mt-4">+ Add Outline</button>
+        </div>
+      </div>
+    </DndContext>
   );
 }
 
